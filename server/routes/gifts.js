9ecd69d6f05e297c59ee858f4b6b298c159b030a
Wisesofthemall <foxxydieujuste@gmail.com>
2024-09-25 22:45:57 -0400

    Completed

--- server/routes/gifts.js
+++ server/routes/gifts.js
@@ -7,10 +7,9 @@ import giftData from "../data/gifts.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = express.Router();
+import GiftsController from "../controllers/gifts.js";
 
-router.get("/", (req, res) => {
-  res.status(200).json(giftData);
-});
+router.get("/", GiftsController.getGifts);
 
 router.get("/:giftId", (req, res) => {
   res
